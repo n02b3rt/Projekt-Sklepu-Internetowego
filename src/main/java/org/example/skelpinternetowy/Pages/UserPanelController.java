fc70fc3ea92a3f762ff1fd22321c3558a1950857
Korratus2003 <95227592+Korratus2003@users.noreply.github.com>
2024-06-17 18:59:53 +0200

    zmienino UserPanel

--- src/main/java/org/example/skelpinternetowy/Pages/UserPanelController.java
+++ src/main/java/org/example/skelpinternetowy/Pages/UserPanelController.java
@@ -1,4 +1,76 @@
 package org.example.skelpinternetowy.Pages;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
+
 public class UserPanelController {
+
+    @FXML
+    private GridPane accionButtons;
+
+    @FXML
+    private Button cancleButton;
+
+    @FXML
+    private Button okButton;
+
+
+
+    @FXML
+    private Button changeButton;
+
+
+    @FXML
+    private TextField address;
+
+    @FXML
+    private TextField email;
+
+    @FXML
+    private TextField name;
+
+    @FXML
+    private TextField nick;
+
+    @FXML
+    private TextField password;
+
+    @FXML
+    private TextField surname;
+
+    @FXML
+    void cancleChanges(MouseEvent event) {
+
+    }
+
+    @FXML
+    void changeData(MouseEvent event) {
+
+    }
+
+    @FXML
+    void modifyData(MouseEvent event){
+        accionButtons.setVisible(true);
+        changeButton.setVisible(false);
+
+
+        address.setEditable(true);
+        address.setStyle("-fx-background-color: #ffffff;");
+        email.setEditable(true);
+        email.setStyle("-fx-background-color: #ffffff;");
+        name.setEditable(true);
+        name.setStyle("-fx-background-color: #ffffff;");
+        nick.setEditable(true);
+        nick.setStyle("-fx-background-color: #ffffff;");
+        password.setEditable(true);
+        password.setStyle("-fx-background-color: #ffffff;");
+        surname.setEditable(true);
+        surname.setStyle("-fx-background-color: #ffffff;");
+
+
+    }
+
 }
